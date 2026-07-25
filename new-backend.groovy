242e9217669769f0da5a002d59dcbad04688d36a
ayush <ayushkamble820@gmail.com>
2026-07-25 21:01:37 +0530

    pipeline changed

--- new-backend.groovy
+++ new-backend.groovy
@@ -28,7 +28,7 @@ pipeline {
             steps{
                 withCredentials([
                             usernamePassword(
-                                credentialsId: 'docker-hub-creds',
+                                credentialsId: 'dockerhub_creds',
                                 usernameVariable: 'DOCKER_USERNAME',
                                 passwordVariable: 'DOCKER_PASSWORD'
                             )
